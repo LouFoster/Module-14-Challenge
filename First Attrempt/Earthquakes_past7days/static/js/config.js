2965cda96807dc5865f75961f2ba9bde8d587a88
Lou (Lawuana) Foster <117233641+LouFoster@users.noreply.github.com>
2023-04-08 18:20:34 -0500

    Add files via upload

---
+++ First Attrempt/Earthquakes_past7days/static/js/config.js
@@ -0,0 +1,2 @@
+//  API Key
+const API_KEY = "pk.eyJ1IjoidGlnZ2VydGF6MzY5IiwiYSI6ImNsZzZ0eWtjdjBndnAzbW84N3piZWFxYmIifQ.LFavGnA8pQe1tqJNAcabBQ"
---
+++ Mapping_Earthquakes/static/css/leaf.js
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+crossorigin=""/>
+
+<div id="map"></div>
+
+#map { height: 180px; }
+
+var map = L.map('map').setView([51.505, -0.09], 13);
